879c20c036ebadfcd47b4082f52e3a286a858c15
Your Name <you@example.com>
2022-04-16 19:23:38 +0200

    products page added. Navbar and footer created

---
+++ js/base-script.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', menus);
+
+
+//This function makes the menus collapsable
+function menus() {
+
+    let links = document.getElementById('mobile-menu'); // Lo que oculto
+    document.addEventListener('click', toggleLinks); // Lo que hago
+
+    // Menu with mobile screen
+    function toggleLinks() {
+
+        let hamburger = document.getElementById('hamburger');
+        if (!(hamburger.offsetParent === null)) {
+            links.classList.toggle('hidden');
+        }
+
+    }
+
+    // Menu with whide screen
+    document.getElementById('user-menu-button')
+        .addEventListener('click', () => {
+            let menu = document.querySelector('[role=menu]');
+            menu.classList.toggle('hidden');
+        });
+
+
+
+}
+
+
